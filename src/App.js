f44710f64921c60e5dfec7ca5723d7872b5e8ab0
James Capps <jcapps@protonmail.com>
2019-07-15 13:17:13 -0500

    Removed props and semi colon from super.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Tone from 'tone'
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super()
       this.state = {
                     name: 'Make a beat!', 
                     color: ""
